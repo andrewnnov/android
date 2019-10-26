be6b3ead7727240766eb6461b846191daf5e21fc
andreybannikov <andrewnnov@yandex.ru>
2019-10-26 16:24:47 +0300

    add if for negative value

--- MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
+++ MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void decreaseButton(View view) {
         quantity = quantity - 1;
+        if(quantity < 0) {
+            quantity = 0;
+        }
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
     }
