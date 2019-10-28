3cf72f0165e144a31d22d23f26e2852505202cb6
andreybannikov <andrewnnov@yandex.ru>
2019-10-28 07:32:01 +0300

    add new pics and refactor

--- MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
+++ MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
@@ -4,41 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int quantity = 0;
     Spinner spinner;
     ArrayList spinnerArrayList;
     //позволяет связать спинер а ArrayList
     ArrayAdapter spinnerAdapter;
+    String goodsName;
+    double price;
+
+    HashMap goodsMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = findViewById(R.id.spinner);
-        spinnerArrayList = new ArrayList();
-        spinnerArrayList.add("guitar");
-        spinnerArrayList.add("drums");
-        spinnerArrayList.add("keyboard");
-
-        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinner.setAdapter(spinnerAdapter);
+        createSpinner();
+        createMap();
 
 
+
+
+    }
+
+    void createMap() {
+        goodsMap = new HashMap();
+        goodsMap.put("guitar", 500.0);
+        goodsMap.put("drums", 1500.0);
+        goodsMap.put("keyboard", 1000.0);
     }
 
     public void increaseQuantity(View view) {
         quantity = quantity + 1;
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
+
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
     }
 
     public void decreaseButton(View view) {
@@ -48,5 +60,59 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
+
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //присваиваем переменной выбор из спинера
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double)goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+
+        switch (goodsName) {
+            case "guitar":
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+
+            case "drums":
+                goodsImageView.setImageResource(R.drawable.drums);
+                break;
+
+            case "keyboard":
+                goodsImageView.setImageResource(R.drawable.keyboard);
+                break;
+
+            default:
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+
+        }
+    }
+
+    void createSpinner() {
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(spinnerAdapter);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
