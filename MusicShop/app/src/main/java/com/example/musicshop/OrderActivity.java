3fe1889ea2989e71f37c3c7491e0165fbabc261a
andreybannikov <andrewnnov@yandex.ru>
2019-10-30 07:37:09 +0300

    add sent to email

--- MusicShop/app/src/main/java/com/example/musicshop/OrderActivity.java
+++ MusicShop/app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -3,10 +3,16 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String[] addresses = {"spnn2012@gmail.com"};
+    String subject = "Order from music shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,30 @@ public class OrderActivity extends AppCompatActivity {
         int quantity = receivedOrderIntent.getIntExtra("quantity", 0);
         double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0.0);
         double price = receivedOrderIntent.getDoubleExtra("price", 0.0);
-        TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer Name: " + userName + "\n"  +
+
+        emailText = "Customer Name: " + userName + "\n"  +
                 "Goods name: " + goodsName + "\n" +
                 "Quantity: " + quantity + "\n" +
                 "Price : " + price + "\n" +
-                "Oder price: " + orderPrice);
+                "Oder price: " + orderPrice;
+
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText(emailText);
     }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
+
+    }
+
+
 }
