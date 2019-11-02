bcd4d0427765b8772345abf27fa79a0b30f32524
andreybannikov <andrewnnov@yandex.ru>
2019-11-02 08:21:38 +0300

    create animation with scale and rotation

--- TomandJerry/app/src/main/java/com/example/tomandjerry/MainActivity.java
+++ TomandJerry/app/src/main/java/com/example/tomandjerry/MainActivity.java
@@ -14,24 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ImageView tomImageView = findViewById(R.id.tomImageView);
+
+        //tomImageView.animate().rotation(360).setDuration(3000);
+        //tomImageView.animate().scaleY(0.5f).scaleX(0.5f).rotation(3600).setDuration(3000);
+
+        //tomImageView.animate().translationXBy(-100).translationYBy(-100).setDuration(3000);
     }
 
     public void eraseTom(View view) {
 
         if(isTomVisible) {
             ImageView tomImageView = findViewById(R.id.tomImageView);
-            tomImageView.animate().alpha(0).setDuration(3000);
+            tomImageView.animate().alpha(0).rotation(tomImageView.getRotation() + 3600)
+                    .scaleX(0)
+                    .scaleY(0)
+                    .setDuration(3000);
 
             ImageView jerryImageView = findViewById(R.id.jerryImageView);
-            jerryImageView.animate().alpha(1).setDuration(3000);
+            jerryImageView.animate().alpha(1).rotation(jerryImageView.getRotation() +  3600)
+                    .scaleX(1)
+                    .scaleY(1)
+                    .setDuration(3000);
 
             isTomVisible = false;
         } else {
             ImageView tomImageView = findViewById(R.id.tomImageView);
-            tomImageView.animate().alpha(1).setDuration(3000);
+            tomImageView.animate().alpha(1).rotation(tomImageView.getRotation() + 3600)
+                    .scaleX(1)
+                    .scaleY(1)
+                    .setDuration(3000);
 
             ImageView jerryImageView = findViewById(R.id.jerryImageView);
-            jerryImageView.animate().alpha(0).setDuration(3000);
+            jerryImageView.animate().alpha(0).rotation(jerryImageView.getRotation() +  3600)
+                    .scaleX(0)
+                    .scaleY(0)
+                    .setDuration(3000);
 
             isTomVisible = true;
 
